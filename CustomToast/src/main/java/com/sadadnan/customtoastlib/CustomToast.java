da050ee66ac2efc2e25c9437ec9af64b32f1273b
MD. Sad Adnan <mail2sad.bd@gmail.com>
2021-03-12 14:19:49 +0600

    forth commit

--- CustomToast/src/main/java/com/sadadnan/customtoastlib/CustomToast.java
+++ CustomToast/src/main/java/com/sadadnan/customtoastlib/CustomToast.java
@@ -13,18 +13,18 @@ import androidx.cardview.widget.CardView;
 public class CustomToast {
     public static void ShowSuccessToast(Context mContext, boolean isLong, String successMessage) {
         Toast toast = new Toast(mContext);
-        if (isLong){
+        if (isLong) {
             toast.setDuration(Toast.LENGTH_LONG);
-        }else{
+        } else {
             toast.setDuration(Toast.LENGTH_SHORT);
         }
 
         //inflate view
-        View custom_view = ((AppCompatActivity)mContext).getLayoutInflater().inflate(R.layout.toast_icon_text, null);
+        View custom_view = ((AppCompatActivity) mContext).getLayoutInflater().inflate(R.layout.toast_icon_text, null);
         ((TextView) custom_view.findViewById(R.id.message)).setText(successMessage);
 
         ((ImageView) custom_view.findViewById(R.id.icon)).setImageResource(R.drawable.ic_done);
-        ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity)mContext).getResources().getColor(R.color.green500));
+        ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity) mContext).getResources().getColor(R.color.green500));
 
         toast.setView(custom_view);
         toast.show();
@@ -32,18 +32,18 @@ public class CustomToast {
 
     public static void ShowErrorToast(Context mContext, boolean isLong, String errorMessage) {
         Toast toast = new Toast(mContext);
-        if (isLong){
+        if (isLong) {
             toast.setDuration(Toast.LENGTH_LONG);
-        }else{
+        } else {
             toast.setDuration(Toast.LENGTH_SHORT);
         }
 
         //inflate view
-        View custom_view = ((AppCompatActivity)mContext).getLayoutInflater().inflate(R.layout.toast_icon_text, null);
+        View custom_view = ((AppCompatActivity) mContext).getLayoutInflater().inflate(R.layout.toast_icon_text, null);
         ((TextView) custom_view.findViewById(R.id.message)).setText(errorMessage);
 
         ((ImageView) custom_view.findViewById(R.id.icon)).setImageResource(R.drawable.ic_close);
-        ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity)mContext).getResources().getColor(R.color.red500));
+        ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity) mContext).getResources().getColor(R.color.red500));
 
         toast.setView(custom_view);
         toast.show();
@@ -51,33 +51,33 @@ public class CustomToast {
 
     public static void ShowInfoToast(Context mContext, boolean isLong, String infoMessage) {
         Toast toast = new Toast(mContext);
-        if (isLong){
+        if (isLong) {
             toast.setDuration(Toast.LENGTH_LONG);
-        }else{
+        } else {
             toast.setDuration(Toast.LENGTH_SHORT);
         }
 
         //inflate view
-        View custom_view = ((AppCompatActivity)mContext).getLayoutInflater().inflate(R.layout.toast_icon_text, null);
+        View custom_view = ((AppCompatActivity) mContext).getLayoutInflater().inflate(R.layout.toast_icon_text, null);
         ((TextView) custom_view.findViewById(R.id.message)).setText(infoMessage);
 
         ((ImageView) custom_view.findViewById(R.id.icon)).setImageResource(R.drawable.ic_error_outline);
-        ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity)mContext).getResources().getColor(R.color.blue500));
+        ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity) mContext).getResources().getColor(R.color.blue500));
 
         toast.setView(custom_view);
         toast.show();
     }
 
-    public static void showToastWithCustomDrawableAndBG(Context mContext, boolean isLong, String message,int drawableRes,int colorRes){
+    public static void showToastWithCustomDrawableAndBG(Context mContext, boolean isLong, String message, int drawableRes, int colorRes) {
         Toast toast = new Toast(mContext);
-        if (isLong){
+        if (isLong) {
             toast.setDuration(Toast.LENGTH_LONG);
-        }else{
+        } else {
             toast.setDuration(Toast.LENGTH_SHORT);
         }
 
         //inflate view
-        View custom_view = ((AppCompatActivity)mContext).getLayoutInflater().inflate(R.layout.toast_icon_text, null);
+        View custom_view = ((AppCompatActivity) mContext).getLayoutInflater().inflate(R.layout.toast_icon_text, null);
         ((TextView) custom_view.findViewById(R.id.message)).setText(message);
 
         try {
@@ -87,10 +87,10 @@ public class CustomToast {
             ((ImageView) custom_view.findViewById(R.id.icon)).setImageResource(R.drawable.ic_error_outline);
         }
         try {
-            ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity)mContext).getResources().getColor(colorRes));
+            ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity) mContext).getResources().getColor(colorRes));
         } catch (Resources.NotFoundException e) {
             e.printStackTrace();
-            ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity)mContext).getResources().getColor(R.color.blue500));
+            ((CardView) custom_view.findViewById(R.id.parent_view)).setCardBackgroundColor(((AppCompatActivity) mContext).getResources().getColor(R.color.blue500));
         }
 
         toast.setView(custom_view);
