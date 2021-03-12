fa125b6e528f7c02f02b22ef76e7a874dcf1c939
MD. Sad Adnan <mail2sad.bd@gmail.com>
2021-03-12 13:59:50 +0600

    second commit

--- CustomToast/src/androidTest/java/com/sadadnan/customtoastlib/ExampleInstrumentedTest.java
+++ CustomToast/src/androidTest/java/com/sadadnan/customtoastlib/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.sadadnan.customtoastlib.test", appContext.getPackageName());
+        assertEquals("com.sadadnan.customtoastproject.test", appContext.getPackageName());
     }
 }
